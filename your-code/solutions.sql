22cc7ef4d8bb75624f16f6731fdf1882ab7338bf
David Aparicio <apariciofernandez.d@gmail.com>
2023-04-26 18:17:11 +0200

    3.3-lab_mysql_advanced_CHALLENGE2_v1

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -26,3 +26,36 @@ GROUP BY
 		sq_1.`TITLE_ID`
     ;
 ---------
+(step 3 no he conseguido ejecutarlo)
+
+
+
+Challenge 2.
+
+CREATE TEMPORARY TABLE temp_1
+
+SELECT titles.title_id AS TITLE_ID,
+		authors.au_id AS AUTHOR_ID,
+        titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS SALES_ROYALTIES
+FROM titles
+		LEFT JOIN titleauthor ON titles.title_id = titleauthor.title_id
+        LEFT JOIN authors ON titleauthor.au_id = authors.au_id
+        LEFT JOIN sales ON titles.title_id = sales.title_id
+        ;
+---------
+CREATE TEMPORARY TABLE temp_2
+
+SELECT temp_1.`TITLE_ID`,
+		temp_1.`AUTHOR_ID`,
+        SUM(temp_1.`SALES_ROYALTIES`) AS AGGREGATED_ROYALTIES
+FROM temp_1
+GROUP BY
+		temp_1.`AUTHOR_ID`,
+		temp_1.`TITLE_ID`
+		;
+---------
+
+
+
+Challenge 3.
+
