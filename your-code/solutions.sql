b77569f7474ecae844ae5aa45ba9cf11450665e4
Daniel Villanueva <dfvt1906@gmail.com>
2023-04-26 17:17:54 +0200

    [Daniel Villanueva][3.3-lab_mysql_advanced]

---
+++ your-code/solutions.sql
@@ -0,0 +1,30 @@
+/*paso 1*/
+
+SELECT titles.title_id, authors.au_id, titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS sales_royalty
+
+FROM titles
+
+LEFT JOIN sales ON titles.title_id = sales.title_id
+
+LEFT JOIN titleauthor ON sales.title_id = titleauthor.title_id
+
+LEFT JOIN authors ON titleauthor.au_id = authors.au_id;
+
+/*paso 2*/
+
+SELECT titleauthor.au_id, titles.title_id, SUM(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS sales_royalty
+
+FROM titleauthor
+
+JOIN titles ON titles.title_id = titleauthor.title_id
+
+JOIN sales ON sales.title_id = titles.title_id
+
+GROUP BY titleauthor.au_id, titles.title_id
+
+/*paso 3*/
+
+ORDER BY sales_royalty desc
+
+LIMIT 3;
+
